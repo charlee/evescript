6b96cd091ee835165f39e7852d9945a6b3ca6bf2
Charlee Li <oda.charlee@gmail.com>
2021-03-20 13:44:02 -0400

    bumped version to 0.2.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*names, **kwargs):
 
 setup(
     name='evescript',
-    version='0.2.0',
+    version='0.2.1',
     license='MIT',
     description='A simple script language for event-based automation tasks.',
     long_description='%s\n%s' % (
